40001795b739ed69293256ee30e65d2100b8ed3d
jumabay <abduhalikovzuma@gmail.com>
2021-04-01 20:51:15 +0600

    lesson #1

---
+++ juma_dz_1.py
@@ -0,0 +1,8 @@
+# Поработайте с переменными, создайте несколько, выведите на экран.
+# Запросите у пользователя некоторые числа и строки и сохраните в переменные,
+# затем выведите на экран.
+
+name = input(" What is your name:")
+print(f"Hello, {name.title()}")
+age = int(input(" How old are you:"))
+print(f"I am {age} years old")
